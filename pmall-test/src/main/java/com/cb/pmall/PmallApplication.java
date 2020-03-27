1014bb1a37a71f468a08103906f95df16d6d126e
chenbin <547788718@qq.com>
2020-03-27 11:09:49 +0800

    PhoneMall第一次测试

---
+++ pmall-test/src/main/java/com/cb/pmall/PmallApplication.java
@@ -0,0 +1,13 @@
+package com.cb.pmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmallApplication.class, args);
+    }
+
+}
---
+++ pmall-test/src/test/java/com/cb/pmall/PmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.pmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
