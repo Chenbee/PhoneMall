0f6eabc7870e756e39b9c7c7f9b1b75d77ded3e5
ChenBee <547788718@qq.com>
2020-04-06 00:22:14 +0800

    商品管理第一次提交

---
+++ pmall-item-web/src/test/java/com/cb/pmall/item/PmallItemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.pmall.item;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmallItemWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
